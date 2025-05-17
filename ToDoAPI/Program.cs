8a0b7d3e6c0ae1bc979743b81c2f6db79bcf5de3
Nguyen Minh Quang <quangnmse184116@fpt.edu.vn>
2025-05-17 09:24:05 +0700

    Adding Model + Controller for TodoList

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
